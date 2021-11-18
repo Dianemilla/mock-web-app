f334a9d5deee616ddf24081e0db7041ca5ef6071
Sandbox Sync <support@getsandbox.com>
2021-11-18 08:19:12 +0000

    Changes

--- authcode/main.mjs
+++ authcode/main.mjs
@@ -331,11 +331,7 @@ Sandbox.define('/callback/token','GET', function(req, res) {
     });
 })
 
-Sandbox.define('/data-masking','GET', function(req, res) {
-    // Check the request, make sure it is a compatible type
-    if (!req.is('application/json')) {
-        return res.send(400, 'Invalid content type, expected application/json');
-    }
+Sandbox.define('/data-masking','GET', function(req, res){
     
     // Set the type of response, sets the content type.
     res.type('application/json');
